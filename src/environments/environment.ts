f3b4f3b6070da17fecf01573a704d71077ceccbb
Alexey <alexeyshalunov@gmail.com>
2020-03-02 15:34:42 +0200

    Update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,17 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-
-  firebase: {
-    apiKey: "AIzaSyC-IerVzHUn-zwcKcFqehJgSPc0JbehEhE",
-    authDomain: "angularex-c0a1e.firebaseapp.com",
-    databaseURL: "https://angularex-c0a1e.firebaseio.com",
-    projectId: "angularex-c0a1e",
-    storageBucket: "angularex-c0a1e.appspot.com",
-    messagingSenderId: "231616037232",
-    appId: "1:231616037232:web:38644e35238bd8ab264fb5"
-  }
+  production: false
     
 };
 
